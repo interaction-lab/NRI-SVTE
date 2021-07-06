fd873569025a602e813d7ec400ca4c310f28dd64
Ipek Goktan <ipekg@usc.edu>
2021-07-06 12:46:44 -0700

    added MobileBaseDriver messages

---
+++ RosSharp_Test/Assets/Scripts/MobileSensorSubscriber.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using RosSharp.RosBridgeClient.MessageTypes.MobileBaseDriver;
+namespace RosSharp.RosBridgeClient
+{
+    public class MobileSensorSubscriber : UnitySubscriber<MessageTypes.MobileBaseDriver.Sensors>
+    {
+        //public MeshRenderer meshRenderer;
+        public Transform PublishedTransform;
+        private Vector3 position;
+
+        private MessageTypes.MobileBaseDriver.Touch touch;
+        private bool isMessageReceived;
+
+        protected override void Start()
+        {
+            base.Start();
+    }
+        private void Update()
+        {
+            //Debug.Log("test");
+            if (isMessageReceived)
+                ProcessMessage();
+        }
+
+        protected override void ReceiveMessage(MessageTypes.MobileBaseDriver.Sensors message)
+        {
+            //Debug.Log("got a message!");
+            this.touch = message.touch;
+            isMessageReceived = true;
+        }
+
+        private void ProcessMessage()
+        {
+            //Debug.Log("pm");
+            if (touch.electrodes[0])
+            {
+                Debug.Log("FL");
+            }
+            if (touch.electrodes[1])
+            {
+                Debug.Log("F");
+            }
+            if (touch.electrodes[2])
+            {
+                Debug.Log("RL");
+            }
+            if (touch.electrodes[3])
+            {
+                Debug.Log("C");
+            }
+            if (touch.electrodes[0])
+            {
+                Debug.Log("FL");
+            }
+            isMessageReceived = false;
+        }
+    }
+}
