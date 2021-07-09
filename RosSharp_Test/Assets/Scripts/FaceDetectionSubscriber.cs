08a5f1f4a79ba660d85917c222aa7db28a79293d
Ipek Goktan <ipekg@usc.edu>
2021-07-09 15:18:27 -0700

    Added lerp and scaling to make the face detection smoother

--- RosSharp_Test/Assets/Scripts/FaceDetectionSubscriber.cs
+++ RosSharp_Test/Assets/Scripts/FaceDetectionSubscriber.cs
@@ -4,12 +4,18 @@ namespace RosSharp.RosBridgeClient
 {
     public class FaceDetectionSubscriber : UnitySubscriber<MessageTypes.Vision.FrameResults>
     {
-        //public MeshRenderer meshRenderer;
         public Transform PublishedTransform;
         private Vector3 position;
+        private Vector3 targetPosition;
+        private Vector3 scale;
 
         private MessageTypes.Vision.FaceArray faces;
         private bool isMessageReceived;
+
+        private float lerpDuration = 3;
+        private float startValue = 0;
+        private float endValue = 10;
+        private float valueToLerp;
 
         protected override void Start()
         {
@@ -20,6 +26,8 @@ namespace RosSharp.RosBridgeClient
             //Debug.Log("test");
             if (isMessageReceived)
                 ProcessMessage();
+            LerpPosition(targetPosition, 5);
+
         }
 
         protected override void ReceiveMessage(MessageTypes.Vision.FrameResults message)
@@ -35,10 +43,27 @@ namespace RosSharp.RosBridgeClient
             if (faces.faces.Length > 0)
             {
                 Debug.Log("I see a face!");
-                position = GetPosition(faces.faces[0]);
-                PublishedTransform.position = position;
+                targetPosition = GetPosition(faces.faces[0]);
+                scale = GetScale(faces.faces[0]);
+                
+                PublishedTransform.localScale = scale;
             }
             isMessageReceived = false;
+        }
+
+        private void Lerp(float timeElapsed)
+        {
+            valueToLerp = Mathf.Lerp(startValue, endValue, 1);
+                //timeElapsed / lerpDuration);
+        }
+
+        private void LerpPosition(Vector3 targetPosition, float duration)
+        {
+            Vector3 startPosition = PublishedTransform.position;
+
+            position = Vector3.Lerp(startPosition, targetPosition, 1);
+            position.z = PublishedTransform.position.z;
+            PublishedTransform.position = position;
         }
 
         private Vector3 GetPosition(MessageTypes.Vision.Face message)
@@ -48,6 +73,14 @@ namespace RosSharp.RosBridgeClient
                 5 + (-5 * (float)message.center.y),
                 //(float)message.center.z);
                 PublishedTransform.position.z);
+        }
+
+        private Vector3 GetScale(MessageTypes.Vision.Face message)
+        {
+            return new Vector3(
+                (float)(message.bb[3] * 0.01),
+                (float)(message.bb[2] * 0.01),
+                PublishedTransform.localScale.z);
         }
     }
 }
