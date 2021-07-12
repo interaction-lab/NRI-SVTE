f2a54610129f30bf7fccca808779acb6faa1cbfe
Karen Berba <berbak@oregonstate.edu>
2021-07-12 11:21:14 -0700

    Added left, right, forward, and backward functionality to Arrow

---
+++ RosSharp_Test/Assets/Scripts/MoveArrow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveArrow : MonoBehaviour
+{
+    //private Rigidbody arrowRb;
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //// get the arrow's rigidbody
+        //arrowRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get input from up and down arrow keys
+        float forwardInput = Input.GetAxis("Vertical");
+
+        // get input from left and right arrow keys
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+
+        // move arrow left or right
+        transform.Translate(Vector3.left * Time.deltaTime * speed * horizontalInput);
+
+        // move arrow forward or backward
+        transform.Translate(Vector3.up * Time.deltaTime * speed * forwardInput);
+
+    }
+}
