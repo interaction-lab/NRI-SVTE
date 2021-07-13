253e6576ba247ac81abd70fd39e90420eaea7851
Ipek Goktan <ipekg@usc.edu>
2021-07-12 17:30:29 -0700

    the text manager can now display various prompts

--- RosSharp_Test/Assets/Scripts/KuriTextManager.cs
+++ RosSharp_Test/Assets/Scripts/KuriTextManager.cs
@@ -7,11 +7,36 @@ using TMPro;
 
 public class KuriTextManager : MonoBehaviour
 {
-    public Text changingText;
-    public GameObject changingTextTwo;
+    public GameObject kuriText;
+    private int currentTaskLoc;
+
+    private string[] congratulation = {
+                    "Well done!",
+                    "Good job!",
+                    "Great!",
+                    "Excellent!",
+                    "Very good!"
+                };
+    private string[] encouragement = {
+                    "You can do this!",
+                    "Go ahead!",
+                    "Don't give up!",
+                    "Keey trying!"
+                };
+
+    private string[] tasks = {
+                    "Use the controller on the top left to move me around!",
+                    "Try turning on my camera display!",
+                    "Look at my face detection display, this is how I see you!",
+                    "Give me a pet on the head!",
+                    "Try pushing in my bumper",
+                    "Pick me up!",
+                };
+
 
     void Start()
     {
+        currentTaskLoc = 0;
     }
 
 
@@ -19,13 +44,16 @@ public class KuriTextManager : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            if (currentTaskLoc + 1 < tasks.Length)
+                currentTaskLoc++;
+            else
+                currentTaskLoc = 0;
             ChangeText();
         }
     }
 
     public void ChangeText()
     {
-        changingText.text = "changed!";
-        changingTextTwo.GetComponent<TextMeshPro>().text = "diff change";
+        kuriText.GetComponent<TextMeshPro>().text = tasks[currentTaskLoc];
     }
 }
