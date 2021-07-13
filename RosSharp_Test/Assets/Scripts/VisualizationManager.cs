8bd5caf50f2c809e825815c846f9607ba7fd238f
Ipek Goktan <ipekg@usc.edu>
2021-07-13 16:04:12 -0700

    made toggle code more dynamic

--- RosSharp_Test/Assets/Scripts/VisualizationManager.cs
+++ RosSharp_Test/Assets/Scripts/VisualizationManager.cs
@@ -6,59 +6,36 @@ namespace RosSharp_Test
 {
     public class VisualizationManager : Singleton<VisualizationManager>
     {
-        public MeshRenderer meshFrontLeft;
-        public MeshRenderer meshLeft;
-        public MeshRenderer meshRearLeft;
-        public MeshRenderer meshCenter;
-        public MeshRenderer meshFront;
-        public MeshRenderer meshRearRight;
-        public MeshRenderer meshRight;
-        public MeshRenderer meshFrontRight;
-        public MeshRenderer kuriTextBackground;
-        public MeshRenderer kuriText;
-        public MeshRenderer KuriCameraManager;
-        public MeshRenderer userFace;
-
-        //public Transform ct;
-        //public MeshRenderer[] CapTouch = GetComponentsInChildren<MeshRenderer>(ct);
+       
+        public Transform NextStepButton;
+        public Transform CapTouch;
+        public Transform KuriCamera;
+        public Transform KuriText;
 
         void Start()
         {
             //CapTouch.enabled = false;
         }
 
-        void Update()
-        {
-
-        }
-
         public void toggleCapTouch()
         {
-            /*
-            foreach (Transform child in GetComponentsInChildren<Transform>(ct))
-            {
-                child.gameObject.SetActive(false);
-            }*/
-            meshFrontLeft.enabled = !meshFrontLeft.enabled;
-            meshLeft.enabled = !meshLeft.enabled;
-            meshRearLeft.enabled = !meshRearLeft.enabled;
-            meshCenter.enabled = !meshCenter.enabled;
-            meshFront.enabled = !meshFront.enabled;
-            meshRearRight.enabled = !meshRearRight.enabled;
-            meshRight.enabled = !meshRight.enabled;
-            meshFrontRight.enabled = !meshFrontRight.enabled;
+            CapTouch.gameObject.SetActive(!CapTouch.gameObject.activeSelf);
         }
 
         public void toggleKuriText()
         {
-            kuriText.enabled = !kuriText.enabled;
-            kuriTextBackground.enabled = !kuriTextBackground.enabled;
+            KuriText.gameObject.SetActive(!KuriText.gameObject.activeSelf);
         }
 
         public void toggleKuriCameraManager()
         {
-            KuriCameraManager.enabled = !KuriCameraManager.enabled;
-            userFace.enabled = !userFace.enabled;
+
+            KuriCamera.gameObject.SetActive(!KuriCamera.gameObject.activeSelf);
+        }
+
+        public void toggleNext()
+        {
+            NextStepButton.gameObject.SetActive(!NextStepButton.gameObject.activeSelf);
         }
     }
 }
