b88c8c2c03db324dc355dd8c35b39d8ca9892a9f
Ipek Goktan <ipekg@usc.edu>
2021-07-13 10:46:05 -0700

    turned text manager script into singleton

--- RosSharp_Test/Assets/Scripts/KuriTextManager.cs
+++ RosSharp_Test/Assets/Scripts/KuriTextManager.cs
@@ -1,30 +1,32 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 
-
-public class KuriTextManager : MonoBehaviour
+namespace RosSharp_Test
 {
-    public GameObject kuriText;
-    private int currentTaskLoc;
+    public class KuriTextManager : Singleton<KuriTextManager> 
+    {
+        public GameObject kuriText;
+        private int currentTaskLoc;
 
-    private string[] congratulation = {
+        private string[] congratulation = {
                     "Well done!",
                     "Good job!",
                     "Great!",
                     "Excellent!",
                     "Very good!"
                 };
-    private string[] encouragement = {
+        private string[] encouragement = {
                     "You can do this!",
                     "Go ahead!",
                     "Don't give up!",
                     "Keey trying!"
                 };
 
-    private string[] tasks = {
+        private string[] tasks = {
                     "Use the controller on the top left to move me around!",
                     "Try turning on my camera display!",
                     "Look at my face detection display, this is how I see you!",
@@ -34,38 +36,39 @@ public class KuriTextManager : MonoBehaviour
                 };
 
 
-    void Start()
-    {
-        currentTaskLoc = 0;
-    }
-
-
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Space))
+        void Start()
         {
-            if (currentTaskLoc + 1 < tasks.Length)
-                currentTaskLoc++;
-            else
-                currentTaskLoc = 0;
-            PrintTask(currentTaskLoc);
+            currentTaskLoc = 0;
         }
+
+
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (currentTaskLoc + 1 < tasks.Length)
+                    currentTaskLoc++;
+                else
+                    currentTaskLoc = 0;
+                PrintTask(currentTaskLoc);
+            }
+        }
+
+        public void PrintTask(int loc)
+        {
+            kuriText.GetComponent<TextMeshPro>().text = tasks[loc];
+        }
+
+        public void PrintEnc(int loc)
+        {
+            kuriText.GetComponent<TextMeshPro>().text = encouragement[loc];
+        }
+
+        public void PrintCong(int loc)
+        {
+            kuriText.GetComponent<TextMeshPro>().text = congratulation[loc];
+        }
+
+
     }
-
-    public void PrintTask(int loc)
-    {
-        kuriText.GetComponent<TextMeshPro>().text = tasks[loc];
-    }
-
-    public void PrintEnc(int loc)
-    {
-        kuriText.GetComponent<TextMeshPro>().text = encouragement[loc];
-    }
-
-    public void PrintCong(int loc)
-    {
-        kuriText.GetComponent<TextMeshPro>().text = congratulation[loc];
-    }
-
-
 }
