b29f6d55cdfcbd8aa83cac5f5162aba43675d4e1
Karen Berba <berbak@oregonstate.edu>
2021-07-18 17:57:11 -0700

    Created an array of bumper sensor statuses that track each sensor's current collision status

--- RosSharp_Test/Assets/Scripts/CollisionColorChange.cs
+++ RosSharp_Test/Assets/Scripts/CollisionColorChange.cs
@@ -7,7 +7,7 @@ public class CollisionColorChange : MonoBehaviour
     public Color sensorOn;
     public Color sensorOff;
     public int bumperPosition;  // right = 0, middle = 1, left = 2
-    public bool pressedStatus;
+    public int[] bumperSensors = new int[] { 0, 0, 0 };  // 0 = sensor not bumped ; 1 = sensor bumped
 
 
     // Start is called before the first frame update
@@ -16,13 +16,12 @@ public class CollisionColorChange : MonoBehaviour
         // default is sensor off
         transform.GetComponent<Renderer>().material.color = sensorOff;
 
-        pressedStatus = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Debug.Log("Bumper sensor status: " + bumperSensors[0] + " " + bumperSensors[1] + " " + bumperSensors[2]);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,21 +30,31 @@ public class CollisionColorChange : MonoBehaviour
         if (other.gameObject.CompareTag("Obstacle"))
         {
             // print out a message indicating which sensor has been bumped
-            Debug.Log(gameObject.name + " has been bumped");
+            Debug.Log(gameObject.name + " position " + bumperPosition + " has been bumped");
 
             // change the color of the sensor to green 
             transform.GetComponent<Renderer>().material.color = sensorOn;
-            pressedStatus = true;
+
+            // change sensor status to bumped
+            bumperSensors[bumperPosition] = 1;
+
+            // print sensor status after being bumped
+            Debug.Log("Bumper sensor status: " + bumperSensors[0] + " " + bumperSensors[1] + " " + bumperSensors[2]);
+
         }
 
     }
 
     private void OnTriggerExit(Collider other)
     {
-        // default is sensor off
+        // when sensor is no longer touching the obstacle
         transform.GetComponent<Renderer>().material.color = sensorOff;
 
-        pressedStatus = false;
+        // change sensor status to not bumped
+        bumperSensors[bumperPosition] = 0;
+
+        // print sensor status after no longer touching obstacle
+        Debug.Log("Bumper sensor status: " + bumperSensors[0] + " " + bumperSensors[1] + " " + bumperSensors[2]);
     }
 
 }
