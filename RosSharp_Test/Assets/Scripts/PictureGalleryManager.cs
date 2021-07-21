95503fe464e560a3c30b223e01f093dfabff661c
Tom Groechel <groechel@usc.edu>
2021-07-20 22:01:27 -0700

    Added appbar & bounding box closes #45

--- RosSharp_Test/Assets/Scripts/PictureGalleryManager.cs
+++ RosSharp_Test/Assets/Scripts/PictureGalleryManager.cs
@@ -4,30 +4,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace KuriSVTE {
-    public class PictureGalleryManager : Singleton<PictureGalleryManager> {
+namespace KuriSVTE
+{
+    public class PictureGalleryManager : Singleton<PictureGalleryManager>
+    {
         List<GameObject> PictureList { get; set; } = new List<GameObject>();
 
         private ImageSubscriber _imageSubscriber;
         private AnimationPublisher animPub;
-        public ImageSubscriber ImageSubscriber {
-            get {
-                if (!_imageSubscriber) {
+        public ImageSubscriber ImageSubscriber
+        {
+            get
+            {
+                if (!_imageSubscriber)
+                {
                     _imageSubscriber = FindObjectOfType<ImageSubscriber>();
                 }
                 return _imageSubscriber;
             }
         } // TODO: replace inspector public variable with better getter
 
-        public void TakePicture() {
+        public void TakePicture()
+        {
             animPub.PublishAnim(AnimationPublisher.ANIMATION_CMD.gotit);
-            PictureList.Add(Instantiate(Resources.Load<GameObject>(ResourcePathConstants.PictureFrame)));
+            PictureList.Add(Instantiate(Resources.Load<GameObject>(ResourcePathConstants.PictureObject)));
             GameObject curPic = PictureList[PictureList.Count - 1];
             curPic.transform.SetParent(transform);
             curPic.transform.localPosition = Vector3.forward; // maybe we can aim this at where the person is looking...
-            curPic.transform.Rotate(new Vector3(0, 0, 180)); // fixes inversion issue
+            // curPic.transform.Rotate(new Vector3(0, 0, 180)); // fixes inversion issue TODO: move this down
 
-            MeshRenderer rend = curPic.GetComponent<MeshRenderer>();
+            MeshRenderer rend = curPic.transform.GetChild(1).GetComponent<MeshRenderer>(); // TODO: get rid of child
             Texture2D canvasTexture = new Texture2D(1, 1);
             canvasTexture.LoadImage(ImageSubscriber.ImageData);
             canvasTexture.Apply();
--- RosSharp_Test/Assets/Scripts/ResourcePathConstants.cs
+++ RosSharp_Test/Assets/Scripts/ResourcePathConstants.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace KuriSVTE {
-    public class ResourcePathConstants {
+namespace KuriSVTE
+{
+    public class ResourcePathConstants
+    {
         public static string PrefabFolder = "Prefabs/";
 
         public static string PictureFrame = PrefabFolder + "PictureFrame";
+        public static string PictureObject = PrefabFolder + "PictureObject";
     }
 }
