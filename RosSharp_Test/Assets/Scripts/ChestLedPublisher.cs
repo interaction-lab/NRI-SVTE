6f483689a26c4896436fb0f51505ab305fd8ac82
Ipek Goktan <ipekg@usc.edu>
2021-07-21 16:40:13 -0700

    merged compilation-hl12 to fix animation errors
    not a very sophisticated merge, the conflicts were too complex for the web browser to display them

--- RosSharp_Test/Assets/Scripts/ChestLedPublisher.cs
+++ RosSharp_Test/Assets/Scripts/ChestLedPublisher.cs
@@ -19,7 +19,7 @@ namespace RosSharp.RosBridgeClient {
 
         private void FixedUpdate() {
             if (colorPicker != null && colorPicker.isActiveAndEnabled) {
-                Color c = colorPicker.CustomColor;
+                Color c = new Color();
                 setColor((int)(c.r * 255), (int)(c.g * 255), (int)(c.b * 255));
             }
 
