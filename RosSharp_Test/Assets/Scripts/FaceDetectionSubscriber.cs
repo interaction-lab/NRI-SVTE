33ebc2a0890c49bfd674d708f1284c4cc1488eaa
tgroechel <groechel@usc.edu>
2021-07-21 20:37:01 -0700

    Lots of fixes

--- RosSharp_Test/Assets/Scripts/FaceDetectionSubscriber.cs
+++ RosSharp_Test/Assets/Scripts/FaceDetectionSubscriber.cs
@@ -1,9 +1,7 @@
 using UnityEngine;
 using RosSharp.RosBridgeClient.MessageTypes.Vision;
-namespace RosSharp.RosBridgeClient
-{
-    public class FaceDetectionSubscriber : UnitySubscriber<MessageTypes.Vision.FrameResults>
-    {
+namespace RosSharp.RosBridgeClient {
+    public class FaceDetectionSubscriber : UnitySubscriber<MessageTypes.Vision.FrameResults> {
         public Transform PublishedTransform;
         private Vector3 position;
         private Vector3 targetPosition;
@@ -12,7 +10,15 @@ namespace RosSharp.RosBridgeClient
 
         private MessageTypes.Vision.FaceArray faces;
         private bool isMessageReceived;
-        private AnimationPublisher animPub;
+        private AnimationPublisher _animPub;
+        public AnimationPublisher AnimPublisher {
+            get {
+                if (!_animPub) {
+                    _animPub = FindObjectOfType<AnimationPublisher>();
+                }
+                return _animPub;
+            }
+        } // TODO: replace inspector public variable with better getter
 
         private float lerpDuration = 3;
         private float startValue = 0;
@@ -21,52 +27,45 @@ namespace RosSharp.RosBridgeClient
         private bool userThere;
         private bool prevUserThere;
 
-        protected override void Start()
-        {
+        protected override void Start() {
             base.Start();
             userThere = false;
             prevUserThere = false;
         }
-        private void Update()
-        {
+        private void Update() {
             if (isMessageReceived)
                 ProcessMessage();
             LerpPosition(targetPosition, 5);
             LerpScale(targetScale, 5);
         }
 
-        protected override void ReceiveMessage(MessageTypes.Vision.FrameResults message)
-        {
+        protected override void ReceiveMessage(MessageTypes.Vision.FrameResults message) {
             this.faces = message.faces;
             isMessageReceived = true;
         }
 
-        private void ProcessMessage()
-        {
-            if (faces.faces.Length > 0)
-            {
+        private void ProcessMessage() {
+            if (faces.faces.Length > 0) {
                 userThere = true;
-                if(!prevUserThere)
-                    animPub.PublishAnim(AnimationPublisher.ANIMATION_CMD.face_detected);
+                if (!prevUserThere)
+                    AnimPublisher.PublishAnim(AnimationPublisher.ANIMATION_CMD.face_detected);
                 targetPosition = GetPosition(faces.faces[0]);
-                targetScale = GetScale(faces.faces[0]);                
-            }else
-            {
+                targetScale = GetScale(faces.faces[0]);
+            }
+            else {
                 userThere = false;
                 if (prevUserThere)
-                    animPub.PublishAnim(AnimationPublisher.ANIMATION_CMD.face_lost);
+                    AnimPublisher.PublishAnim(AnimationPublisher.ANIMATION_CMD.face_lost);
             }
             prevUserThere = userThere;
             isMessageReceived = false;
         }
 
-        private void Lerp(float timeElapsed)
-        {
+        private void Lerp(float timeElapsed) {
             valueToLerp = Mathf.Lerp(startValue, endValue, 1);
         }
 
-        private void LerpPosition(Vector3 targetPosition, float duration)
-        {
+        private void LerpPosition(Vector3 targetPosition, float duration) {
             Vector3 startPosition = PublishedTransform.position;
 
             position = Vector3.Lerp(startPosition, targetPosition, 1);
@@ -74,8 +73,7 @@ namespace RosSharp.RosBridgeClient
             PublishedTransform.position = position;
         }
 
-        private void LerpScale (Vector3 targetScale, float duration)
-        {
+        private void LerpScale(Vector3 targetScale, float duration) {
             Vector3 startScale = PublishedTransform.localScale;
 
             scale = Vector3.Lerp(startScale, targetScale, 1);
@@ -83,16 +81,14 @@ namespace RosSharp.RosBridgeClient
             PublishedTransform.localScale = scale;
         }
 
-        private Vector3 GetPosition(MessageTypes.Vision.Face message)
-        {
+        private Vector3 GetPosition(MessageTypes.Vision.Face message) {
             return new Vector3(
                 (float)message.center.x,
                 (float)message.center.y,
                 PublishedTransform.position.z);
         }
 
-        private Vector3 GetScale(MessageTypes.Vision.Face message)
-        {
+        private Vector3 GetScale(MessageTypes.Vision.Face message) {
             return new Vector3(
                 (float)(message.bb[3] * 0.001),
                 (float)(message.bb[2] * 0.001),
--- RosSharp_Test/Assets/Scripts/HeadPanPublisher.cs
+++ RosSharp_Test/Assets/Scripts/HeadPanPublisher.cs
@@ -1,80 +1,51 @@
 using UnityEngine;
 
-namespace RosSharp.RosBridgeClient
-{
-    public class HeadPanPublisher : UnityPublisher<MessageTypes.Trajectory.JointTrajectoryPoint>
-    {
-        private MessageTypes.Trajectory.JointTrajectoryPoint message;
-        private double panLeft = 0.78;
-        private double panRight = -0.78;
-        private double tiltUp = -0.92;
-        private double tiltDown = 0.29;
-        private double[] positions;
-        private double moveCoef = 0.1;
+namespace RosSharp.RosBridgeClient {
+    public class HeadPanPublisher : UnityPublisher<MessageTypes.Trajectory.JointTrajectoryPoint> {
+        private MessageTypes.Trajectory.JointTrajectoryPoint msg;
 
-        protected override void Start()
-        {
+        string JOINT_PAN = "head_1_joint", JOINT_TILT = "head_2_joint", JOINT_EYES = "eyelids_joint";
+        static float JOINT_HEIGHT = 0.405f, PAN_LEFT = 0.78f, PAN_NEUTRAL = 0f;
+        static float PAN_RIGHT = -PAN_LEFT, TILT_UP = -0.92f, TILT_NEUTRAL = 0.0f;
+        static float TILT_DOWN = 0.29f, EYES_OPEN = 0.0f, EYES_NEUTRAL = 0.1f;
+        static float EYES_CLOSED = 0.41f, EYES_HAPPY = -0.16f, EYES_SUPER_SAD = 0.15f, EYES_CLOSED_BLINK = 0.35f;
+        string HEAD_NS = "head_controller/follow_joint_trajectory", EYES_NS = "eyelids_controller/follow_joint_trajectory";
+
+
+        /*       private double panLeft = 0.78;
+               private double panRight = -0.78;
+               private double tiltUp = -0.92;
+               private double tiltDown = 0.29;
+               private double[] positions;
+               private double moveCoef = 0.1;*/
+
+        protected override void Start() {
             base.Start();
-            positions = new double[2];
-            positions[0] = 0;
-            positions[1] = 0;
             InitializeMessage();
         }
 
-        private void FixedUpdate()
-        {
-            UpdateMessage();
+        private void InitializeMessage() {
+            msg = new MessageTypes.Trajectory.JointTrajectoryPoint();
         }
 
-        private void InitializeMessage()
-        {
-            message = new MessageTypes.Trajectory.JointTrajectoryPoint();
-            message.positions = positions;
-            Publish(message);
+        public void moveUp() {
 
         }
-        private void UpdateMessage()
-        {
+
+        public void moveDown() {
+
         }
 
-        public void moveUp()
-        {
-            if(positions[1] - moveCoef > tiltUp)
-            {
-                positions[1] -= moveCoef;
-                message.positions = positions;
-                Publish(message);
-            }
+        public void moveRight() {
+
         }
 
-        public void moveDown()
-        {
-            if (positions[1] + moveCoef < tiltDown)
-            {
-                positions[1] += moveCoef;
-                message.positions = positions;
-                Publish(message);
-            }
-        }
-
-        public void moveRight()
-        {
-            if (positions[0] - moveCoef > panRight)
-            {
-                positions[0] -= moveCoef;
-                message.positions = positions;
-                Publish(message);
-            }
-        }
-
-        public void moveLeft()
-        {
-            if (positions[0] + moveCoef < panLeft)
-            {
+        public void moveLeft() {
+            /*if (positions[0] + moveCoef < panLeft) {
                 positions[0] += moveCoef;
-                message.positions = positions;
-                Publish(message);
-            }
+                msg.positions = positions;
+                Publish(msg);
+            }*/
         }
     }
 }
