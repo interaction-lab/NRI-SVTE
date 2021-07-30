30b30da7a5dbd739a5ddfe6f2ded94fefb50ca76
Karen Berba <berbak@oregonstate.edu>
2021-07-30 14:57:51 -0700

    Each photo cube contains a screenshot texture taken by the eyes camera

--- RosSharp_Test/Assets/Scripts/VirtualPhotoGallery.cs
+++ RosSharp_Test/Assets/Scripts/VirtualPhotoGallery.cs
@@ -3,15 +3,13 @@ using RosSharp_Test;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 namespace KuriSVTE
 {
     public class VirtualPhotoGallery : Singleton<VirtualPhotoGallery>
     {
         List<GameObject> PictureList { get; set; } = new List<GameObject>();
-        public Texture2D pictureTexture;
+        public Camera pictureCamera;
         public GameObject picture;
-
         public float zCoord = 0f;
 
         public void TakePicture()
@@ -27,20 +25,18 @@ namespace KuriSVTE
             // changes rotation of the picture cube
             curPic.transform.localRotation = Quaternion.Euler(0, -90, 0);
 
-            // holds the updated picture texture
-            Texture2D updatedTexture = pictureTexture;
-
             // sets the texture onto the cube
             MeshRenderer rend = curPic.transform.GetChild(0).GetComponent<MeshRenderer>(); // TODO: get rid of child
-            Texture2D canvasTexture = new Texture2D(pictureTexture.width, pictureTexture.height);
-            canvasTexture.SetPixels(pictureTexture.GetPixels());
+            Texture2D canvasTexture = new Texture2D(pictureCamera.activeTexture.width, pictureCamera.activeTexture.height);
+            RenderTexture.active = pictureCamera.activeTexture;
+            canvasTexture.ReadPixels(new Rect(0, 0, pictureCamera.activeTexture.width, pictureCamera.activeTexture.height), 0, 0);
             canvasTexture.Apply();
+            RenderTexture.active = null;
             rend.material = new Material(Shader.Find("Mixed Reality Toolkit/Standard")); //             TODO: move this to resource path constants
             rend.material.SetTexture("_MainTex", canvasTexture);
 
             // increments the z coordinate so each new picture taken is placed side by side
             zCoord -= 0.27f;
-
         }
     }
 }
