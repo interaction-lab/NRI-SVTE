499829ce73c1496c7a44c2e87f50e59ed44ae198
a-oconnell <ao71627@usc.edu>
2021-09-14 15:06:27 -0700

    Add RayCasting to RayCaster

--- RosSharp_Test/Assets/Scripts/RayCaster.cs
+++ RosSharp_Test/Assets/Scripts/RayCaster.cs
@@ -10,20 +10,30 @@ public class RayCaster : MonoBehaviour
   public int numberOfObjects = 181;
   public float radius = 5f;
   public float MaxRotation = Mathf.PI/2;
+    public float weaponRange = 50f;
 
 
-  void Start()
+    void Start()
   {
+      RaycastHit hit;
+
       for (int i = -90; i < 90; i++)
       {
           float angle = i * Mathf.PI / 180;
           //float x = Mathf.Cos(angle) * radius;
           //float z = Mathf.Sin(angle) * radius;
-          //Vector3 pos = transform.position + new Vector3(x, 0, z);
           float angleDegrees = 180 - angle*Mathf.Rad2Deg;
           Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
           Instantiate(prefab, transform.position, rot, transform);
+
+          int layermask = 1 << 6;
+          layermask = ~layermask;
+
+            Physics.Raycast(transform.position, rot * transform.up, out hit, weaponRange, layermask);
+            //print(out.hit);
           // Instantiate(prefab, transform, rot);
       }
-  }
+  
+
+    }
 }
