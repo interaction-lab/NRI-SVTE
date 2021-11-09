13924e06f7a7c1ca7309821986740dc44d7e5c51
a-oconnell <ao71627@usc.edu>
2021-11-09 15:36:51 -0800

    TrailRender demo works

--- RosSharp_Test/Assets/Scripts/ProjectileMotion.cs
+++ RosSharp_Test/Assets/Scripts/ProjectileMotion.cs
@@ -4,22 +4,89 @@ using UnityEngine;
 
 public class ProjectileMotion : MonoBehaviour
 {
-    public float movementDelta = .10f;
-    private float pos = 0f;
-    // Start is called before the first frame update
-    void Start()
-    {
+  public float movementDelta = .10f;
+  private float pos = 0f;
+  public AnimationCurve animCurve;
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    }
+  }
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Alpha0))
     {
-        transform.position += transform.forward * movementDelta;
-        pos += movementDelta;
-        if (pos >= 5){
-          transform.position -= transform.forward * pos;
-          pos = 0f;
-        }
+      StartCoroutine(MoveBall());
     }
+  }
+
+  IEnumerator MoveBall() {
+    GetComponent<TrailRenderer>().emitting = true;
+    float totalTime = 1;
+    float returnTime = .25f;
+    float curtime = 0;
+    Vector3 startPose = transform.position;
+    Vector3 endPose = transform.position + Vector3.up * 3; // 0, 1, 0
+    while(curtime < totalTime - .25)
+    {
+      transform.position = Vector3.Lerp(startPose, endPose, animCurve.Evaluate(curtime/totalTime));
+      curtime += Time.deltaTime;
+      yield return new WaitForSeconds(Time.deltaTime);
+    }
+    GetComponent<TrailRenderer>().emitting = false;
+    while(curtime < totalTime)
+    {
+      transform.position = Vector3.Lerp(startPose, endPose, animCurve.Evaluate(curtime/totalTime));
+      curtime += Time.deltaTime;
+      yield return new WaitForSeconds(Time.deltaTime);
+    }
+    curtime = 0;
+    while(curtime < returnTime)
+    {
+      transform.position = Vector3.Lerp(endPose, startPose, animCurve.Evaluate(curtime/returnTime));
+      curtime += Time.deltaTime;
+      yield return new WaitForSeconds(Time.deltaTime);
+    }
+    // GetComponent<TrailRenderer>().emitting = false;
+    // yield return new WaitForSeconds(Time.deltaTime);
+    transform.position = startPose;
+    StartCoroutine(MoveBall());
+
+  }
+
 }
+
+// public class LerpSphere : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     public AnimationCurve animCurve;
+//
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if (Input.GetKeyDown(KeyCode.Alpha0))
+//         {
+//             StartCoroutine(MoveBall());
+//         }
+//     }
+//
+//     IEnumerator MoveBall() {
+//         GetComponent<TrailRenderer>().enabled = true;
+//         float totalTime = 1;
+//         float curtime = 0;
+//         Vector3 startPose = transform.position;
+//         Vector3 endPose = transform.position + Vector3.up * 3; // 0, 1, 0
+//         while(curtime < totalTime)
+//         {
+//             transform.position = Vector3.Lerp(startPose, endPose, animCurve.Evaluate(curtime/totalTime));
+//             curtime += Time.deltaTime;
+//             yield return new WaitForSeconds(Time.deltaTime);
+//         }
+//         GetComponent<TrailRenderer>().enabled = false;
+//         transform.position = startPose;
+//         StartCoroutine(MoveBall());
+//
+//     }
+// }
