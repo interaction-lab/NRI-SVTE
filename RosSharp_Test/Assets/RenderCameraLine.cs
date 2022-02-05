64389979622dd855cdbe5897b1456a3e1e85d826
a-oconnell <ao71627@usc.edu>
2022-02-04 18:42:54 -0800

    add drag+drop to plane/line camera viz

--- RosSharp_Test/Assets/RenderCameraLine.cs
+++ RosSharp_Test/Assets/RenderCameraLine.cs
@@ -9,6 +9,7 @@ public class RenderCameraLine : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+      LineRenderer cameraLine = gameObject.AddComponent<LineRenderer>();
 
     }
 
