04e4d6befec8c5756735ddf891c3a136ac0e23a1
tgroechel <groechel@usc.edu>
2022-02-17 10:14:37 -0800

    Cleaned up showlasers

--- RosSharp_Test/Assets/Scripts/ShowLasers.cs
+++ RosSharp_Test/Assets/Scripts/ShowLasers.cs
@@ -3,6 +3,8 @@ using UnityEngine.UI;
 
 namespace NRISVTE {
     public class ShowLasers : MonoBehaviour {
+
+        #region members
         public GameObject prefab;
         public bool StaticLinesEnabled = false;
         public bool StaticPointsEnabled = false;
@@ -11,104 +13,113 @@ namespace NRISVTE {
         public Slider colorSlider;
         public FlexibleColorPicker cp;
 
-        public GameObject[] prefabs;
+        public GameObject[] lidarPrefabArray;
         private Color lidarColor = Color.white;
         private float lidarWidth = 0.00f;
-        float noiseLevel = 0.001f;
+        float noiseLevel = 0.002f;
         float publishHZ = 15, lastTimeStamp = 0, rateInMS = -1;
 
         public float[] rangies;
-
-
-        void Start() {
-            if (widthSlider == null) {
-                // enabled = false;
-                Debug.LogWarning("Disabling ShowLasers as Slider is not set, might be intended for this scene. This script should be migrated to not use public member/drag and drop.");
-                // return;
-            }
-            prefabs = new GameObject[180];
-
-            for (int i = 0; i < 180; i++) {
-                float angle = i * Mathf.PI / 180;
-                float angleDegrees = 180 + angle * Mathf.Rad2Deg;
-                Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
-                GameObject pf = Instantiate(prefab, transform.position, rot, transform) as GameObject;
-                prefabs[i] = pf;
-                LineRenderer laserLine = pf.GetComponent<LineRenderer>();
-                laserLine.enabled = StaticLinesEnabled;
-                TrailRenderer lidarTrail = pf.GetComponent<TrailRenderer>();
-                lidarWidth = widthSlider.value;
-
-
-                lidarTrail.startWidth = lidarWidth;
-            }
-
-            rateInMS = 1000f / publishHZ;
-        }
-
         public class Message {
             public float[] ranges;
             public float maxRange;
         }
 
+        #endregion
+
+        #region Unity
+        void Start() {
+            CreateLidarPrefabArray();
+            rateInMS = 1000f / publishHZ;
+        }
+        #endregion
+
+        #region public
         public void UpdateRanges(Message message) {
             if (Time.time * 1000f - lastTimeStamp < rateInMS) {
                 return;
             }
-            lastTimeStamp = Time.time * 1000f;
 
+            lastTimeStamp = Time.time * 1000f;
             float[] ranges = message.ranges;
             rangies = ranges;
             float maxRange = message.maxRange;
+            transform.localRotation = Quaternion.Euler(0, 135, 0);
             for (int i = 0; i < 180; i++) {
-                GameObject pf = prefabs[i];
-                LineRenderer laserLine = pf.GetComponent<LineRenderer>();
-                laserLine.enabled = StaticLinesEnabled;
-
-                // calculate end point
-                Vector3 pos = pf.transform.position;
-                pos = pos + (pf.transform.forward * ranges[i]);
-                pos.x *= (1 + Random.Range(-noiseLevel, noiseLevel));
-                pos.z *= (1 + Random.Range(-noiseLevel, noiseLevel));
-
-                // Turn on/off points
-                if (StaticPointsEnabled) {
-                    Transform point = pf.transform.GetChild(1);
-                    point.gameObject.SetActive(true);
-                    point.transform.position = pos;
-                }
-                else {
-                    pf.transform.GetChild(1).gameObject.SetActive(false);
-                }
-
-                // Turn on/off trailrenderer
-                Transform sphere = pf.transform.GetChild(0);
-                TrailRenderer lidarTrail = sphere.GetComponent<TrailRenderer>();
-                var PM = sphere.GetComponent<ProjectileMotion>();
-                PM.startPose = pf.transform.position;
-                PM.endPose = pf.transform.position + (pf.transform.forward * maxRange);
-                PM.maxRange = maxRange;
-                PM.range = ranges[i];
-
-                // laser line positions
-                laserLine.startWidth = lidarWidth;
-                laserLine.SetPosition(0, pf.transform.position);
-                laserLine.SetPosition(1, pos);
-                laserLine.material.color = lidarColor;
-                lidarWidth = widthSlider.value;
-                lidarColor = cp.color;
-
-                lidarTrail.startWidth = lidarWidth;
-                lidarTrail.material.color = lidarColor;
-
-
+                GameObject lidarPrefab = lidarPrefabArray[i];
+                Vector3 pos = calculateLocalEndPoint(lidarPrefab, ranges[i]);
+                SetUpStaticPoints(lidarPrefab, pos);
+                SetUpTrailRenderer(ranges, maxRange, i, lidarPrefab, lidarWidth, lidarColor);
+                SetUpLaserLines(lidarPrefab, pos);
             }
         }
 
         public void ToggleAllLidarRenderers(bool on) {
-            foreach (GameObject go in prefabs) {
+            foreach (GameObject go in lidarPrefabArray) {
                 go.SetActive(on);
             }
         }
+        #endregion
+
+        #region private
+        private void CreateLidarPrefabArray() {
+            lidarPrefabArray = new GameObject[180];
+            for (int i = 0; i < 180; i++) {
+                float angle = i * Mathf.PI / 180;
+                float angleDegrees = 180 + angle * Mathf.Rad2Deg;
+                Quaternion rot = Quaternion.Euler(0, angleDegrees, 0);
+                GameObject pf = Instantiate(prefab, transform.position, rot, transform) as GameObject;
+                lidarPrefabArray[i] = pf;
+                LineRenderer laserLine = pf.GetComponent<LineRenderer>();
+                laserLine.enabled = StaticLinesEnabled;
+                TrailRenderer lidarTrail = pf.GetComponent<TrailRenderer>();
+                lidarWidth = widthSlider.value;
+                lidarTrail.startWidth = lidarWidth;
+            }
+        }
+
+        private void SetUpLaserLines(GameObject lidarPrefab, Vector3 pos) {
+            LineRenderer laserLine = lidarPrefab.GetComponent<LineRenderer>();
+            laserLine.enabled = StaticLinesEnabled;
+            laserLine.startWidth = lidarWidth;
+            laserLine.SetPosition(0, transform.position);
+            laserLine.SetPosition(1, pos);
+            laserLine.material.color = lidarColor;
+            lidarWidth = widthSlider.value;
+            lidarColor = cp.color;
+        }
+
+        private void SetUpTrailRenderer(float[] ranges, float maxRange, int i, GameObject lidarPrefab, float lidarWidth, Color lidarColor) {
+            Transform sphere = lidarPrefab.transform.GetChild(0);
+            TrailRenderer lidarTrail = sphere.GetComponent<TrailRenderer>();
+            var PM = sphere.GetComponent<ProjectileMotion>();
+            PM.startPose = lidarPrefab.transform.position;
+            PM.endPose = transform.position + (lidarPrefab.transform.forward * maxRange);
+            PM.maxRange = maxRange;
+            PM.range = ranges[i];
+            lidarTrail.startWidth = lidarWidth;
+            lidarTrail.material.color = lidarColor;
+        }
+
+        private void SetUpStaticPoints(GameObject lidarPrefab, Vector3 pos) {
+            if (StaticPointsEnabled) {
+                Transform point = lidarPrefab.transform.GetChild(1);
+                point.gameObject.SetActive(true);
+                point.transform.position = pos;
+            }
+            else {
+                lidarPrefab.transform.GetChild(1).gameObject.SetActive(false);
+            }
+        }
+
+        private Vector3 calculateLocalEndPoint(GameObject lidarPrefab, float range) {
+            Vector3 pos = transform.position;
+            pos = pos + (lidarPrefab.transform.forward * range);
+            pos.x *= (1 + Random.Range(-noiseLevel, noiseLevel));
+            pos.z *= (1 + Random.Range(-noiseLevel, noiseLevel));
+            return pos;
+        }
+
+        #endregion
     }
 }
