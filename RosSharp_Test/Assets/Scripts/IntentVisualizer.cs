36539f433f3fc0631e3358d4cd32ba6e5cbf938d
a-oconnell <ao71627@usc.edu>
2022-02-25 10:36:41 -0800

    Change colors on intent visualizer

--- RosSharp_Test/Assets/Scripts/IntentVisualizer.cs
+++ RosSharp_Test/Assets/Scripts/IntentVisualizer.cs
@@ -8,14 +8,19 @@ namespace RosSharp.RosBridgeClient
     {
         protected Color[] intentColors;
 
-        
+
         abstract public void Visualize(NLUDataTypes.NLUIntent[] intentList);
 
         abstract public void Disable();
 
         protected void GenerateIntentColors(int length)
         {
-            intentColors = ColorUtility.GetDifferentColors(length);
+            // intentColors = ColorUtility.GetDifferentColors(length);
+
+            intentColors = new Color[2];
+            intentColors[0] = new Color(175, 175, 0, 255);
+            intentColors[1] = new Color(50, 200, 200, 255);
+
         }
 
         public Color GetIntentColor(int index)
@@ -23,6 +28,6 @@ namespace RosSharp.RosBridgeClient
             return intentColors[index];
         }
 
-       
+
     }
 }
