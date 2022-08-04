9ccf571b0fad946f45b6b626c9f816fb440b1b89
Tom Groechel <tgroeche@umich.edu>
2022-08-04 18:34:43 -0400

    wow bts are nice, feeling better about making an interaction

---
+++ RosSharp_Test/Assets/BT/BehaviourTree/Scripts/Composites/MonitorComposite.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using NRISVTE;
+
+namespace TheKiwiCoder {
+    public class MonitorComposite : CompositeNode {
+        List<State> childrenLeftToExecute = new List<State>();
+        List<int> childrenMonitorConditionsIndices = new List<int>();
+
+        protected override void OnStart() {
+            childrenLeftToExecute.Clear();
+            children.ForEach(a => {
+                childrenLeftToExecute.Add(State.Running);
+            });
+            childrenMonitorConditionsIndices.Clear();
+            int i = 0;
+            foreach(var child in children) {
+                if(child is MonitorCondition) {
+                    childrenMonitorConditionsIndices.Add(i);
+                }
+                ++i;
+            }
+        }
+
+        protected override void OnStop() {
+        }
+
+        protected override State OnUpdate() {
+            bool stillRunning = false;
+            for (int i = 0; i < childrenLeftToExecute.Count(); ++i) {
+                if (childrenLeftToExecute[i] == State.Running) {
+                    var status = children[i].Update();
+                    if (status == State.Failure) {
+                        AbortRunningChildren();
+                        return State.Failure;
+                    }
+
+                    if (status == State.Running) {
+                        stillRunning = true;
+                    }
+
+                    childrenLeftToExecute[i] = status;
+                }
+            }
+            // reset all childrenMonitorConditions to running
+            foreach(var index in childrenMonitorConditionsIndices) {
+                childrenLeftToExecute[index] = State.Running;
+            }
+            return stillRunning ? State.Running : State.Success;
+        }
+
+        void AbortRunningChildren() {
+            for (int i = 0; i < childrenLeftToExecute.Count(); ++i) {
+                if (childrenLeftToExecute[i] == State.Running) {
+                    children[i].Abort();
+                }
+            }
+        }
+    }
+}
