9851ae8520d7cb589b001d071009ceda4cd036d8
Tom Groechel <tgroeche@umich.edu>
2022-08-04 18:21:16 -0400

    Runs as person

--- RosSharp_Test/Assets/BT/BehaviourTree/Scripts/Actions/RandomPosition.cs
+++ RosSharp_Test/Assets/BT/BehaviourTree/Scripts/Actions/RandomPosition.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using TheKiwiCoder;
 
-public class RandomPosition : ActionNode
-{
-    public Vector2 min = Vector2.one * -10;
-    public Vector2 max = Vector2.one * 10;
+namespace NRISVTE {
+    public class RandomPosition : ActionNode {
+        public Vector2 min = Vector2.one * -10;
+        public Vector2 max = Vector2.one * 10;
+        KuriTransformManager _kuriTransformManager;
+        KuriTransformManager KuriT {
+            get {
+                if (_kuriTransformManager == null) {
+                    _kuriTransformManager = KuriManager.instance.GetComponent<KuriTransformManager>();
+                }
+                return _kuriTransformManager;
+            }
+        }
 
-    protected override void OnStart() {
-    }
+        protected override void OnStart() {
+        }
 
-    protected override void OnStop() {
-    }
+        protected override void OnStop() {
+        }
 
-    protected override State OnUpdate() {
-        blackboard.goalPosition.x = Random.Range(min.x, max.x);
-        blackboard.goalPosition.z = Random.Range(min.y, max.y);
-        return State.Success;
+        protected override State OnUpdate() {
+            blackboard.goalPosition.x = Random.Range(min.x, max.x);
+            blackboard.goalPosition.y = KuriT.GroundYCord;
+            blackboard.goalPosition.z = Random.Range(min.y, max.y);
+            return State.Success;
+        }
     }
 }
